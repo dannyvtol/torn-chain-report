190cfc8b456c3e237b641c57b6dddb52f5339e54
Danny van Tol <danny.van.tol@outlook.com>
2026-09-03 20:23:51 +0200

    fix(#4): expose GM on globalThis in entry point

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
+import { GM } from "$";
 import { FactionController } from "./modules/faction/FactionController.js";
+
+globalThis.GM = GM;
 
 if (location.pathname.startsWith("/factions.php")) {
     new FactionController().init();
